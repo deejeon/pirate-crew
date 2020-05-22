b0d8ef2a04cfee8b17ad15ca15a7023a1a140663
deejeon <61816883+deejeon@users.noreply.github.com>
2020-05-22 13:26:08 -0700

    ran build

---
+++ client/build/precache-manifest.fc1782a274907ba87be3c8d126f666bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "069c69ea8e6992edc98b1f4eb142362b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb3d76243a002927d30d",
+    "url": "/static/css/main.97a58619.chunk.css"
+  },
+  {
+    "revision": "96e5246c77c2d4a6ebb7",
+    "url": "/static/js/2.7054a53f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7054a53f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb3d76243a002927d30d",
+    "url": "/static/js/main.7c674cbf.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
